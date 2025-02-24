6f9d827b84f9cf64efdfb3faac9105f1ec24ad6f
neominal.solutions <neominal.solutions@gmail.com>
2025-02-24 09:45:56 +0300

    Add project files.

---
+++ src/apps/ConsoleSample/Delagates/Sample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSample.Delagates
+{
+
+  // Delegate Nedir ? Delagate genel olarak bir metodu temsil eden bir türdür.
+  // Delagateler Methodların referansını tutan nesnelerimiz ve bir olay gerçekleştiğinde delegate sayısında bu işlemi başka bir nesneye delege edebiliriz. Sevk edebiliriz.
+  // Event güdümlü programlada tercih ettiğimiz bir geliştirme şeklidir.
+
+  // Bu sayede aynı imzaya sahip olan farklı metotları bir arada tutabiliriz.
+  public delegate void MessageHandler(string message);
+
+  public static class Sample
+  {
+    // FIFO mantığı ile methodları işler.
+    public static void SendEmail(string message)
+    {
+      Console.WriteLine("Email sent: " + message);
+    } 
+
+    public static void SendSms(string message)
+    {
+      Console.WriteLine("Sms sent: " + message);
+    }
+
+
+  }
+}
---
+++ src/apps/ConsoleSample/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using ConsoleSample.Delagates;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World!");
+
+    DelegateSamples();
+
+    MessageHandler del2 = Sample.SendEmail;
+    del2 += Sample.SendSms;
+    Log(del2);
+
+  }
+
+  public static void DelegateSamples()
+  {
+    // Delegate Sample.SendEmail methodundan sorumlu olsun
+    MessageHandler del = Sample.SendEmail;
+    del += Sample.SendSms; // multicast kullanım örneği
+    del("Message1");
+
+    del -= Sample.SendEmail; // subscription kaldırma
+    del("Message2");
+
+  }
+
+  // Delegate başka bir methoda paramete olarak gönderilebilir.
+  // bu durumda işlem sonrası iş akışını devam ettirebiliriz.
+  public static void Log(MessageHandler callback)
+  {
+    Console.WriteLine("Log started");
+    callback("Log message");
+    Console.WriteLine("Log ended");
+  }
+}
+
+
