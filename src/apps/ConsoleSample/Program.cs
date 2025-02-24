f436803fe9cd9d103132e213468d076e050ac2ce
neominal.solutions <neominal.solutions@gmail.com>
2025-02-24 10:16:12 +0300

    Expand Main with delegate examples and logging
    The Main method in the Program class has been significantly expanded to include several examples and usages of delegates in C#. A new region `#region Delegates` has been added to encapsulate the delegate-related code. The DelegateSamples method is now called within the Main method.
    
    Key changes include:
    - Creation and subscription of a MessageHandler delegate to SendEmail and SendSms methods.
    - Dynamic creation of a delegate instance based on user input using Delegate.CreateDelegate.
    - Examples of using Action and Func delegate types.
    - Addition of an anonymous delegate to print a message.
    - Addition of a Log method that demonstrates logging functionality with Action<string, string> delegate.
    - Calls to the Log method with different delegates within the Main method.

--- src/apps/ConsoleSample/Program.cs
+++ src/apps/ConsoleSample/Program.cs
@@ -7,11 +7,72 @@ public class Program
   {
     Console.WriteLine("Hello World!");
 
+    #region Delagates
+
     DelegateSamples();
 
     MessageHandler del2 = Sample.SendEmail;
-    del2 += Sample.SendSms;
+    del2 += Sample.SendSms; // Subscription
     Log(del2);
+
+    // Runtime üzerinden Delegate yapısı kullanıma örnek
+
+    Console.WriteLine("Hangi tipte gönderim yapmak istersiniz ?");
+    string methodName = Console.ReadLine();
+
+    // Not: bu kodu eğer delegate üzerinden dinamik olarak çağıramasaydık aşağıdaki gibi bir if else bloğu yazmamız gerekirdi.
+    var del = (MessageHandler)Delegate.CreateDelegate(typeof(MessageHandler), typeof(Sample), methodName);
+
+    // Bu bağımlı bir sürüdülebilir olmayan bir kod örneği
+    if(methodName == "SendSMS")
+    {
+      Sample.SendSms("Test");
+    }
+    else if(methodName == "SendEmail")
+    {
+      Sample.SendEmail("Test");
+    }
+
+
+    Console.WriteLine("Mesajınızı yazınız");
+    string message = Console.ReadLine();
+
+    ArgumentNullException.ThrowIfNull(del);
+
+    del.DynamicInvoke(message);
+
+    #endregion
+
+
+    // Not: dışardında içerisine parametre alan ve void dönüş tipi için kullanılan bir delegate türüdür. 
+    Action<string, string> concat = (x, y) => {
+      Console.WriteLine($"{x} {y}");
+    }; 
+    concat("Hello", "World");
+
+    //List<string> names = new List<string> { "Ali", "Veli", "Deli" };
+    //names.ForEach(x => Console.WriteLine(x));
+
+    // Function delegate Action delegateden tek farkı geriye bir değer döndürmesidir.
+    Func<int,int,double> avg = (x, y) => (x + y) / 2;
+    double result = avg(9, 6);
+    Console.WriteLine(result);
+
+
+    // en çok kullanıdığı kısım eventlerde kullanılır.
+    // bir işlemden sonra callback döndürmek yani işlem sonrası süreci başka bir işleme devretmek amaçlı kullanılır.
+    // Asenkron programlamada sıklıkla bu tarz anonim delegate tipleri kullanılır.
+    var showMessage = delegate (string message)
+    {
+      Console.WriteLine(message);
+    };
+
+    showMessage("Hello World");
+
+
+    Log(concat);
+
+
 
   }
 
@@ -33,6 +94,13 @@ public class Program
   {
     Console.WriteLine("Log started");
     callback("Log message");
+    Console.WriteLine("Log ended");
+  }
+
+  public static void Log(Action<string,string> callback)
+  {
+    Console.WriteLine("Log started");
+    callback("Hello","Word");
     Console.WriteLine("Log ended");
   }
 }
